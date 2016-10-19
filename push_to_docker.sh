e28bb4cd70947b7cada99e66e18a2cb8f60e2d95
Steven Zeiler <zeiler.steven@gmail.com>
2016-10-18 23:56:41 -0400

    use push_to_docker.sh in circle.yml deploy

--- push_to_docker.sh
+++ push_to_docker.sh
@@ -1,3 +1,3 @@
-docker login -e $DOCKER_EMAIL -p $DOCKER_PASSWORD -u $DOCKER_USER
+docker login -e $DOCKER_EMAIL -p $DOCKER_PASS -u $DOCKER_USER
 docker tag -f arcadiafreshcom_webapp stevenzeiler/arcadiafresh.com:latest
 docker push stevenzeiler/arcadiafresh.com:latest
